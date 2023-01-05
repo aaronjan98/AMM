af9cbb0bbf0a08bd6189b27ce5dbec837c2504d4
aaronjan98 <aaronjan98@gmail.com>
2023-01-05 14:38:11 -0800

    show exchange rates on input elements and token balances on swap form

--- src/components/Swap.js
+++ src/components/Swap.js
@@ -7,18 +7,53 @@ import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
+import { ethers } from 'ethers'
 
 const Swap = () => {
   const [inputToken, setInputToken] = useState(null)
   const [outputToken, setOutputToken] = useState(null)
+  const [inputAmount, setInputAmount] = useState(0)
+  const [outputAmount, setOutputAmount] = useState(0)
 
   const [price, setPrice] = useState(0)
 
   const account = useSelector(state => state.provider.account)
 
   const tokens = useSelector(state => state.tokens.contracts)
+  const symbols = useSelector(state => state.tokens.symbols)
+  const balances = useSelector(state => state.tokens.balances)
 
   const amm = useSelector(state => state.amm.contract)
+
+  const inputHandler = async e => {
+    if (!inputToken || !outputToken) {
+      window.alert('Please select token')
+      return
+    }
+
+    if (inputToken === outputToken) {
+      window.alert('Invalid token pair')
+      return
+    }
+
+    if (inputToken === 'LE') {
+      setInputAmount(e.target.value)
+
+      const _token1Amount = ethers.utils.parseEther(e.target.value)
+      const result = await amm.calculateToken1Swap(_token1Amount)
+      const _token2Amount = ethers.utils.formatEther(result)
+
+      setOutputAmount(_token2Amount.toString())
+    } else {
+      setInputAmount(e.target.value)
+
+      const _token2Amount = ethers.utils.parseEther(e.target.value)
+      const result = await amm.calculateToken2Swap(_token2Amount)
+      const _token1Amount = ethers.utils.formatEther(result)
+
+      setOutputAmount(_token1Amount.toString())
+    }
+  }
 
   const getPrice = async () => {
     if (inputToken === outputToken) {
@@ -27,9 +62,9 @@ const Swap = () => {
     }
 
     if (inputToken === 'LE') {
-      setPrice((await amm.token1Balance()) / (await amm.token2Balance()))
-    } else {
       setPrice((await amm.token2Balance()) / (await amm.token1Balance()))
+    } else {
+      setPrice((await amm.token1Balance()) / (await amm.token2Balance()))
     }
   }
 
@@ -49,7 +84,14 @@ const Swap = () => {
                 <Form.Label>
                   <strong>Input:</strong>
                 </Form.Label>
-                <Form.Text muted>Balance:</Form.Text>
+                <Form.Text muted>
+                  Balance:
+                  {inputToken === symbols[0]
+                    ? balances[0]
+                    : inputToken === symbols[1]
+                    ? balances[1]
+                    : 0}
+                </Form.Text>
               </div>
               <InputGroup>
                 <Form.Control
@@ -57,7 +99,8 @@ const Swap = () => {
                   placeholder="0.0"
                   min="0.0"
                   step="any"
-                  disabled={false}
+                  onChange={e => inputHandler(e)}
+                  disabled={!inputToken}
                 />
                 <DropdownButton
                   variant="outline-secondary"
@@ -82,10 +125,22 @@ const Swap = () => {
                 <Form.Label>
                   <strong>Output:</strong>
                 </Form.Label>
-                <Form.Text muted>Balance:</Form.Text>
+                <Form.Text muted>
+                  Balance:
+                  {outputToken === symbols[0]
+                    ? balances[0]
+                    : outputToken === symbols[1]
+                    ? balances[1]
+                    : 0}
+                </Form.Text>
               </div>
               <InputGroup>
-                <Form.Control type="number" placeholder="0.0" disabled />
+                <Form.Control
+                  type="number"
+                  placeholder="0.0"
+                  value={outputAmount === 0 ? '' : outputAmount}
+                  disabled
+                />
                 <DropdownButton
                   variant="outline-secondary"
                   title="Select Token"
