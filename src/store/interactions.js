88d6350129839d08b56db0636708542b96b1e873
aaronjan98 <aaronjan98@gmail.com>
2023-01-07 13:19:37 -0800

    Implement withdrawals

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -8,6 +8,9 @@ import {
   depositRequest,
   depositSuccess,
   depositFail,
+  withdrawRequest,
+  withdrawSuccess,
+  withdrawFail,
   swapRequest,
   swapSuccess,
   swapFail,
@@ -123,6 +126,21 @@ export const addLiquidity = async (provider, amm, tokens, amounts) => {
   }
 }
 
+/********** REMOVE LIQUIDITY **********/
+export const removeLiquidity = async (provider, amm, shares) => {
+  try {
+    dispatch(withdrawRequest())
+
+    const signer = await provider.getSigner()
+
+    let transaction = await amm.connect(signer).removeLiquidity(shares)
+    await transaction.wait()
+
+    dispatch(withdrawSuccess(transaction.hash))
+  } catch (error) {
+    dispatch(withdrawFail())
+  }
+}
 /**************** SWAP ****************/
 export const swap = async (provider, amm, token, symbol, amount) => {
   try {
